def332f34495bd3499b3bb6388a3375831c49a03
Arnav Srivastava <arnavsrivastava214@gmail.com>
2025-06-30 23:53:27 +0530

    manage admin module added

--- controllers/admin.controller.js
+++ controllers/admin.controller.js
@@ -16,3 +16,80 @@ exports.login = async (req, res) => {
         res.status(400).send({ error: err.message });
     }
 };
+
+
+exports.getAllUsers = async (req, res) => {
+    try {
+        const query = `SELECT * FROM users ORDER BY id DESC`
+        const [customers] = await pool.execute(query);
+        res.send({ message: "users fetched successfully", data: customers, status: 200 });
+    } catch (err) {
+        res.status(500).send({ error: 'Failed to fetch users' });
+    }
+};
+
+exports.createUsers = async (req, res) => {
+    try {
+        let { name, email, phone_number, role, password, pin } = req.body;
+        const [existingUser] = await pool.execute(
+            'SELECT * FROM users WHERE email = ?',
+            [email]);
+        if (existingUser.length > 0) {
+            return res.send({ message: "Email already exists", status: 409 });
+        }
+        pin = Math.floor(100000 + Math.random() * 900000);
+
+        const [result] = await pool.execute(
+            'INSERT INTO users (name, email , phone_number, role ,password , pin) VALUES (?, ?, ? ,?, ?, ?)',
+            [name, email, phone_number, role, password, pin]
+        );
+
+        res.status(201).send({ message: "User created successfully", status: 201 });
+    } catch (err) {
+        console.error(err);
+        res.status(500).send({ error: 'Internal server error', message: err.message, status: 500 });
+    }
+};
+
+
+exports.getUsersById = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const [users] = await pool.execute('SELECT * FROM users WHERE id = ?', [id]);
+
+        if (!users.length) {
+            return res.status(404).send({ message: 'users not found' });
+        }
+
+        res.send(users[0]);
+    } catch (err) {
+        res.status(500).send({ error: 'Failed to get users' });
+    }
+};
+
+exports.updateUsers = async (req, res) => {
+    try {
+        let { name, email, phone_number, role, password, pin} = req.body;
+        const id = req.body.id;
+        pin = Math.floor(100000 + Math.random() * 900000);
+
+        await pool.execute(
+            'UPDATE users SET name = ?, email = ?,phone_number = ?, role = ?, password = ?, pin = ?  WHERE id = ?',
+            [name, email, phone_number, role, password, pin, id]
+        );
+
+        res.send({ message: 'User updated successfully', status:200 });
+    } catch (err) {
+        res.status(500).send({ error: 'Failed to update User', err:err });
+    }
+};
+
+exports.deleteUsers = async (req, res) => {
+    try {
+        const { id } = req.params;
+        await pool.execute('DELETE FROM users WHERE id = ?', [id]);
+        res.send({ message: 'users deleted successfully', status:200 });
+    } catch (err) {
+        res.status(500).send({ error: 'Failed to delete users' });
+    }
+};
--- routes/admin.routes.js
+++ routes/admin.routes.js
@@ -3,6 +3,15 @@ const router = express.Router();
 const adminController = require('../controllers/admin.controller');
 
 router.post('/login', adminController.login);
+router.get('/getAllUsers',adminController.getAllUsers);
+router.post('/createUsers', adminController.createUsers);
+router.get('/getUsersById/:id',adminController.getUsersById);
+router.put('/updateUsers',  adminController.updateUsers);
+router.delete('/deleteUsers/:id', adminController.deleteUsers);
+
+
+
+
 
 
 module.exports = router;
