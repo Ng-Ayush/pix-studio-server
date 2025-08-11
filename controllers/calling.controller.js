094ec2ee9e2110de3fcb8fc6e3f67d8d3b2bce1f
Ng-Ayush <ayushsrivastava1011@gmail.com>
2025-08-12 01:01:23 +0530

    changes

--- controllers/calling.controller.js
+++ controllers/calling.controller.js
@@ -3,11 +3,11 @@ const pool = require('../db_config/db.js');
 
 exports.addAgent = async (req, res) => {
     const { agentName,agentPhone,email } = req.body;
-    const [rows] = await pool.query('INSERT INTO callings (email,agent_name,agent_phone,created_by) VALUES (?, ?, ?, ?)', [email, agentName,agentPhone,req.user.id||0]);
+    const [rows] = await pool.query('INSERT INTO callings (email,agent_name,agent_phone) VALUES (?, ?, ?)', [email, agentName,agentPhone]);
     res.send({ message: 'Agent added successfully', status: 200 });
 };
 
 exports.getAgents = async (req, res) => {
-    const [rows] = await pool.query('SELECT * FROM callings WHERE created_by = ?', [req.user.id || 0]);
+    const [rows] = await pool.query('SELECT * FROM callings');
     res.send({message:"Agents fetched successfully", data:rows ,status:200});
 };
