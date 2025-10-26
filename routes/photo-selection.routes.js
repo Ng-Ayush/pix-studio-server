a2309a8596d2c3ec323cc6ac003e2075e79c7f5c
Ng-Ayush <ayushsrivastava1011@gmail.com>
2025-10-26 15:14:22 +0530

    auth middle ware removed for photo selection

--- routes/photo-selection.routes.js
+++ routes/photo-selection.routes.js
@@ -15,7 +15,7 @@ router.put("/updateEvent/:event_id",authMiddleware, photoSelectionController.upd
 router.get("/getFolderByEventId/:event_id",authMiddleware, photoSelectionController.getFolderByEventId);
 router.get("/getAiGuestByEventId/:event_id",authMiddleware, photoSelectionController.getAiGuestByEventId);
 router.post("/addAiGuest",photoSelectionController.addAiGuest);
-router.get("/getUploadedPhotosByFolderId/:folder_id",authMiddleware, photoSelectionController.getUploadedPhotosByFolderId);
+router.get("/getUploadedPhotosByFolderId/:folder_id", photoSelectionController.getUploadedPhotosByFolderId);
 router.post("/createNewFolder",authMiddleware, photoSelectionController.createNewFolder);
 router.put("/updateFolder/:id",authMiddleware, photoSelectionController.updateFolder);
 router.delete("/deleteFolder/:id",authMiddleware, photoSelectionController.deleteFolder);
